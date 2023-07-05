f3493be69f4455eb7d713a224f00f1c0a521addd
natancarrapeiro <55252815+natancarrapeiro@users.noreply.github.com>
2023-07-05 17:01:10 -0300

    estrutura de repetição for

---
+++ fundamentos_js/js/estruturaRepetição/for.js
@@ -0,0 +1,8 @@
+var idade =0
+for (idade; idade < 18; idade++) {
+    var suaIdade=idade
+    console.log(`Você ainda é de menor de idade so tem ${suaIdade} anos `)
+    if(suaIdade == 17){
+        console.log("ano que vem aos 18 anos  você sera considerado adulto ")
+    }
+}
